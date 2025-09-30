4c3faedef34798873035676962ef93ec39128b68
ito1115 <yucca015@gmail.com>
2025-09-30 13:30:19 +0900

    Herokuの初期デプロイ

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module GraduationApp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    # app/services ディレクトリを自動読み込みパスに追加
+    config.autoload_paths << Rails.root.join('app', 'services')
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
