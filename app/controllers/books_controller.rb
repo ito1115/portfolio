583a839256537c80d5d5390822cd686d6cac1a19
ito1115 <yucca015@gmail.com>
2025-10-19 10:28:14 +0900

    search_view修正,"手動で本を追加"機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,39 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
 
+  # 手動で書籍を追加するフォーム
+  def new
+    @book = Book.new
+  end
+
+  # 手動で書籍を作成
+  def create
+    book_params = params.require(:book).permit(
+      :title, :author, :publisher, :published_date,
+      :description, :isbn, :image_url
+    )
+
+    # 既存の書籍をISBNまたはタイトル+著者で検索
+    @book = if book_params[:isbn].present?
+              Book.find_by(isbn: book_params[:isbn])
+            else
+              Book.find_by(title: book_params[:title], author: book_params[:author])
+            end
+
+    # 存在しなければ新規作成
+    if @book.nil?
+      @book = Book.new(book_params)
+      if @book.save
+        redirect_to new_reading_path(book_id: @book.id), notice: '書籍を追加しました'
+      else
+        render :new, status: :unprocessable_entity
+      end
+    else
+      # 既に存在する場合もそのまま読書記録作成へ
+      redirect_to new_reading_path(book_id: @book.id), notice: 'この書籍は既に登録されています'
+    end
+  end
+
   # Google Books APIで書籍を検索
   def search
     @query = params[:query]
