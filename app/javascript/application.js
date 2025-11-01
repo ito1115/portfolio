e14334dbf6879e88cacef4d1b479debb8db144f3
ito1115 <yucca015@gmail.com>
2025-11-01 15:43:50 +0900

    バーコードリーダー実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { Application } from "@hotwired/stimulus"
 import Autocomplete from "stimulus-autocomplete"
+import BarcodeScannerController from "./controllers/barcode_scanner_controller"
 
 const application = Application.start()
+
+// Register controllers
 application.register("autocomplete", Autocomplete)
+application.register("barcode-scanner", BarcodeScannerController)
