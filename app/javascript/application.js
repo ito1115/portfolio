11e28eac7623353f37feff3cbce8ed53e10227ca
ito1115 <yucca015@gmail.com>
2025-11-03 14:39:18 +0900

    バーコードスキャナーコントローラーをStimulusに登録

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { Application } from "@hotwired/stimulus"
 import Autocomplete from "stimulus-autocomplete"
+import BarcodeScannerController from "controllers/barcode_scanner_controller"
 
 const application = Application.start()
 
 // Register controllers
 application.register("autocomplete", Autocomplete)
-
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+application.register("barcode-scanner", BarcodeScannerController)
