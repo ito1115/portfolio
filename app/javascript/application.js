b9dd3a75e1dcab61642caa8d25e9a606d7bd48d7
ito1115 <yucca015@gmail.com>
2025-11-03 14:18:44 +0900

    Herokuエラー解消

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { Application } from "@hotwired/stimulus"
 import Autocomplete from "stimulus-autocomplete"
-import BarcodeScannerController from "./controllers/barcode_scanner_controller"
 
 const application = Application.start()
 
 // Register controllers
 application.register("autocomplete", Autocomplete)
-application.register("barcode-scanner", BarcodeScannerController)
+
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
