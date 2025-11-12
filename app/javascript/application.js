a54db064d8aea3e9b366b1936a5cfaaa1c001ade
ito1115 <yucca015@gmail.com>
2025-11-12 11:17:56 +0900

    削除確認ダイアログ設定

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
 import { Application } from "@hotwired/stimulus"
 import Autocomplete from "stimulus-autocomplete"
 import BarcodeScannerController from "controllers/barcode_scanner_controller"
