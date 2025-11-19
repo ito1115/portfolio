fe46d01716679a5da918c3fe5eac91d5655e4bb0
ito1115 <yucca015@gmail.com>
2025-11-19 16:26:48 +0900

    TOPページ修正(熟成度説明,homeアクションのリダイレクト削除)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@
 
 class PagesController < ApplicationController
   def home
-    # ログイン済みの場合は読書記録一覧ページにリダイレクト
-    redirect_to readings_path if user_signed_in?
+    # ログイン済みの場合は読書記録一覧ページにリダイレクトすると、ログインユーザーがTOPページを見れないため一旦コメントアウト
+    # redirect_to readings_path if user_signed_in?
   end
 end
