70dc82b0b291cbf84bedb6ff6c8784307fcd6d36
ito1115 <yucca015@gmail.com>
2025-12-23 10:42:47 +0900

    メール送信エラー解消

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@tsundoku.com'
+  default from: ENV.fetch('GMAIL_USERNAME', 'noreply@tsundoku.com')
   layout 'mailer'
 end
